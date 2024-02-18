a035f7c2ff1c238d79afd18f5071c0e14ff5ec98
Raymond Bian <raybbian@gmail.com>
2024-02-18 02:46:37 -0500

    added metadata and updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import {Html, Head, Main, NextScript} from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+                <meta name={"description"} content={"A queue-based to-do list app to motivate myself to solve Codeforces problems daily."}/>
+            </Head>
             <body className={"bg-ctp-crust"}>
             <Main/>
             <NextScript/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import {Inter} from "next/font/google";
 import Header from "@/components/Header";
 import Canvas from "@/components/Canvas";
+import Head from "next/head";
 
 const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
     return (
-        <main className={`h-[100dvh] w-[100dvw] ${inter.className} flex flex-col`}>
-            <Header className={"w-full h-14 flex-none"}/>
-            <Canvas className={"w-full h-full"}/>
-        </main>
+        <>
+            <Head>
+                <title>Landing Queues</title>
+            </Head>
+            <main className={`h-[100dvh] w-[100dvw] ${inter.className} flex flex-col`}>
+                <Header className={"w-full h-14 flex-none"}/>
+                <Canvas className={"w-full h-full"}/>
+            </main>
+        </>
     );
 }
